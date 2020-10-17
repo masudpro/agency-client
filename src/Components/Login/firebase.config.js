416fbff65cf14f9ad9b2168af55608bfd008e0bc
masudpro <masudrana375170@gmail.com>
2020-10-17 15:25:05 +0600

    Secondt

--- src/Components/Login/firebase.config.js
+++ src/Components/Login/firebase.config.js
@@ -1,11 +1,11 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyAvWpcPWhcCJuSvp7TIkjYrrq53lbKORTU",
-  authDomain: "agency-8c7d3.firebaseapp.com",
-  databaseURL: "https://agency-8c7d3.firebaseio.com",
-  projectId: "agency-8c7d3",
-  storageBucket: "agency-8c7d3.appspot.com",
-  messagingSenderId: "139709152241",
-  appId: "1:139709152241:web:8afe83fcff73373a96a250"
+  apiKey: "AIzaSyA2lgyVTVW7aMAoObADi4g4dVbh1whOqUg",
+  authDomain: "august-cascade-220007.firebaseapp.com",
+  databaseURL: "https://august-cascade-220007.firebaseio.com",
+  projectId: "august-cascade-220007",
+  storageBucket: "august-cascade-220007.appspot.com",
+  messagingSenderId: "492088396913",
+  appId: "1:492088396913:web:4474c09d8de1d839395fd0"
 };
 
 export default firebaseConfig;
